3a33d8b499eb279be98d7338ca4a1f5746e53c57
Tanusha <tanusha20020@tut.by>
2021-12-05 20:41:09 +0300

    Add Lab10

---
+++ Lab10_053501_Шурко/FService/FileService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Lab10_053501_Шурко;
+
+namespace FService {
+    public class FileService<T> : IFileService<T> where T : class {
+        public IEnumerable<T> ReadFile(string fileName) {
+            IEnumerable<T> obj = null;
+            try {
+                obj = JsonSerializer.Deserialize<IEnumerable<T>>(File.ReadAllText(fileName));
+                Console.WriteLine("Read file JSON");
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            return obj;
+        }
+        public void SaveData(IEnumerable<T> data, string fileName) {
+            try {
+                File.WriteAllText(fileName, JsonSerializer.Serialize(data, data.GetType()));
+                Console.WriteLine("SerializeJSON");
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
