367faecf1a4182adb0f43312ea136028bc7594ee
Tanusha <tanusha20020@tut.by>
2021-12-05 20:40:16 +0300

    Add Lab11

---
+++ Lab11_053501_Shurko/Lab11_053501_Shurko/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Lab11_053501_Shurko {
+    class Program {
+        public static void DisplayInfo(TimeSpan ts, double res,string name) {
+            Console.WriteLine($"Time in seconds: {ts.TotalSeconds}");
+            Console.WriteLine($"Result: {res}");
+            Console.WriteLine($"Thread name: {name}");
+        }
+        static void Main() {
+            Integral.Calculation += DisplayInfo;
+            Container c2 = new (0, 1);
+            Thread thread2 = new (() => {
+                Task t1 = Integral.CountIntegralAsync(c2);
+                t1.Wait();
+
+            });
+            thread2.Priority = ThreadPriority.Lowest;
+            thread2.Name = "Thread2";
+            Thread thread1 = new (()=>Integral.CountIntegral(c2));
+            thread1.Priority = ThreadPriority.Highest;
+            thread1.Name = "Thread1";
+            Console.WriteLine($"Starts: {thread2.Name}");
+            thread2.Start();
+            Console.WriteLine($"Starts: {thread1.Name}");
+            thread1.Start();
+        }
+    }
+}
---
+++ Lab11_053501_Shurko/Lab11_Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary;
+using System.IO;
+
+namespace Lab11_Task2 {
+    class Program {
+        static async Task Main() {
+            Console.WriteLine("Start writting");
+            StreamService strService = new(100);
+            MemoryStream str = new();
+            Thread thread = new(() => {
+                Task t1 = strService.WriteToStream(str);
+                t1.Wait();
+                });
+            thread.Name = "Thread";
+            Console.WriteLine($"Starts: {thread.Name}");
+            thread.Start();
+            Thread thread1 = new( () => {
+                Task t2 = StreamService.CopyFromStream(str, "result.txt");
+                t2.Wait();
+            });
+            thread1.Name = "Thread1";
+            Console.WriteLine($"Starts: {thread1.Name}");
+            thread1.Start();
+            await Task.WhenAll(StreamService.GetStatisticsAsync("result.txt", (obj) => obj?.Name.Length > 5));
+        }
+    }
+}
