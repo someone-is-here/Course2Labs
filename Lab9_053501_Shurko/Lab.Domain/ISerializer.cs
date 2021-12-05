03193d993ddda1d1795abeeaa8a2ac33fec74324
Tanusha <tanusha20020@tut.by>
2021-12-05 20:55:03 +0300

    Add Lab9

---
+++ Lab9_053501_Shurko/Lab.Domain/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.Domain {
+    public interface ISerializer {
+        IEnumerable<Restaurant> DeSerializeByLINQ(string fileName);
+        IEnumerable<Restaurant> DeSerializeByXML(string fileName);
+        IEnumerable<Restaurant> DeSerializeByJSON(string fileName);
+        void SerializeByLINQ(IEnumerable<Restaurant> restaurant, string fileName);
+        void SerializeXML(IEnumerable<Restaurant> restaurant, string fileName);
+        void SerializeJSON(IEnumerable<Restaurant> restaurant, string fileName);
+    }
+}
