ca8f55c02ddadf1dc739ef008c5e45c66a2865be
Tanusha <tanusha20020@tut.by>
2021-12-13 21:13:04 +0300

    Add Lab11

---
+++ Lab11_053501_Shurko/ClassLibrary/StreamService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassLibrary {
+    public class StreamService {
+        private readonly List<Galery> galeryList;
+        private static int id = 0;
+        public StreamService(int size) {
+            galeryList = new List<Galery>(size);
+            for (int i = 0; i < size; i++) {
+                galeryList.Add(GenInfo());
+            }
+        }
+        private static Galery GenInfo() {
+            Random rand = new ();
+            string[] names = { "Alexey", "Alexsandr", "Harry", "Nikita", "Daniil", "Ekaterina", "Violet", "Veronika" };
+            return new Galery(id++, rand.Next(0, 1000), names[rand.Next(names.Length)]);
+        }
+        public async Task WriteToStream(Stream stream) {
+            await Task.Run(() => {
+                lock (stream) {
+                    Console.WriteLine("WriteToStream() begin");
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                    foreach (Galery gal in galeryList) {
+                        byte[] arr = System.Text.Encoding.UTF8.GetBytes(gal.ToString());
+                        stream.WriteAsync(arr, 0, arr.Length);
+                    }
+                Console.WriteLine("WriteToStream() end");
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                }
+            });
+
+        }
+        public async static Task CopyFromStream(Stream stream, string fileName) {
+            await Task.Run(() => {
+                lock (stream) {
+                    Console.WriteLine("CopyFromStream() begin");
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+                    using (FileStream fstream = new(fileName, FileMode.OpenOrCreate)) {
+                        stream.Position = 0;
+                        try {
+                            Console.WriteLine("StreamL: "+stream.Length);
+                            stream.CopyTo(fstream);
+                        } catch (Exception ex) {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+                Console.WriteLine("CopyFromStream() end");
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            });
+        }
+        public async static Task<int> GetStatisticsAsync(string fileName, Func<Galery, bool> filter) {       
+            int res = 0;
+            Console.WriteLine("GetStatistic");
+            var text = await File.ReadAllTextAsync(fileName);
+            string[] arrayWithValues = text.Split('\n');
+            List<Galery> galery = new();
+            for (int i = 0; i < arrayWithValues.Length - 3; i += 3)
+            {
+                Galery obj = Galery.Parse(arrayWithValues[i], arrayWithValues[i + 2], arrayWithValues[i + 1]);
+                galery.Add(obj);
+            }
+            Console.WriteLine("---------------");
+            foreach (Galery g in galery)
+            {
+                Console.WriteLine(g);
+            }
+            res = ((IEnumerable<Galery>)galery).Count<Galery>(filter);
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+}
---
+++ Lab11_053501_Shurko/Lab11_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary;
+using System.IO;
+
+namespace Lab11_Task2 {
+    class Program {
+        static async Task Main() {
+            Console.WriteLine("Start writting");
+            StreamService strService = new(100);
+            MemoryStream str = new();
+            Task t1 = strService.WriteToStream(str);
+            await Task.Delay(100);
+            Task t2 = StreamService.CopyFromStream(str, "result.txt");
+            await Task.WhenAll(new Task[] { t1, t2 });
+            await StreamService.GetStatisticsAsync("result.txt", (obj) => obj?.Name.Length > 5);
+        }
+    }
+}
